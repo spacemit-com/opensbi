a3d4c1b349948a7fb1bebb3b7ad05da8e1d5a24f
zhouxl <zhouxl@spacemit.com>
2023-03-30 10:57:29 +0800

    get plic/clint timer address from dtb
    Change-Id: I074604f948dce951930be3de7eac8d389a47f667

--- platform/spacemit/k1-pro/platform.c
+++ platform/spacemit/k1-pro/platform.c
@@ -6,64 +6,40 @@
 
 #include <sbi/riscv_asm.h>
 #include <sbi/riscv_encoding.h>
+#include <sbi/riscv_io.h>
 #include <sbi/sbi_const.h>
 #include <sbi/sbi_platform.h>
-#include <sbi/riscv_io.h>
 
 /*
  * Include these files as needed.
  * See objects.mk PLATFORM_xxx configuration parameters.
  */
+#include <sbi_utils/fdt/fdt_helper.h>
 #include <sbi_utils/ipi/aclint_mswi.h>
 #include <sbi_utils/irqchip/plic.h>
+#include <sbi_utils/serial/fdt_serial.h>
 #include <sbi_utils/serial/uart8250.h>
 #include <sbi_utils/timer/aclint_mtimer.h>
-#include <sbi_utils/serial/fdt_serial.h>
 
 #include "platform.h"
 #include <libfdt.h>
 
-#define PLATFORM_PLIC_ADDR          0xB0000000
-#define PLATFORM_PLIC_NUM_SOURCES   128
-#define PLATFORM_HART_COUNT         4
-#define PLATFORM_CLINT_ADDR         0xB4000000
-#define PLATFORM_ACLINT_MTIMER_FREQ 24000000
-#define PLATFORM_ACLINT_MSWI_ADDR   (PLATFORM_CLINT_ADDR + \
-                     CLINT_MSWI_OFFSET)
+#define PLATFORM_PLIC_ADDR 0xB0000000
+#define PLATFORM_PLIC_NUM_SOURCES 128
+#define PLATFORM_HART_COUNT 4
+#define PLATFORM_CLINT_ADDR 0xB4000000
+#define PLATFORM_ACLINT_MTIMER_FREQ 25000000
+#define PLATFORM_ACLINT_MSWI_ADDR (PLATFORM_CLINT_ADDR + \
+                                   CLINT_MSWI_OFFSET)
 #define PLATFORM_ACLINT_MTIMER_ADDR (PLATFORM_CLINT_ADDR + \
-                     CLINT_MTIMER_OFFSET)
-#define PLATFORM_UART_ADDR          0x10010000
-#define PLATFORM_UART_INPUT_FREQ    24000000
-#define PLATFORM_UART_BAUDRATE      115200
+                                     CLINT_MTIMER_OFFSET)
 
-int suart_serial_init(void);
 int qemu_mode;
 
-unsigned long fw_platform_init(unsigned long arg0, unsigned long arg1,
-				unsigned long arg2, unsigned long arg3,
-				unsigned long arg4)
-{
-	const char *compatible;
-	void *fdt = (void *)arg1;
-	int root_offset, len;
-
-	root_offset = fdt_path_offset(fdt, "/");
-	if (root_offset >= 0)
-    {
-	    compatible = fdt_getprop(fdt, root_offset, "compatible", &len);
-        if (compatible &&
-            (0 == sbi_strncmp(compatible, "riscv-virtio", 12)))
-            qemu_mode = 1;
-    }
-
-	/* Return original FDT pointer */
-	return arg1;
-}
-
-//static struct plic_data plic = {
-//  .addr = PLATFORM_PLIC_ADDR,
-//  .num_src = PLATFORM_PLIC_NUM_SOURCES,
-//};
+static struct plic_data plic = {
+    .addr = PLATFORM_PLIC_ADDR,
+    .num_src = PLATFORM_PLIC_NUM_SOURCES,
+};
 
 static struct aclint_mswi_data mswi = {
     .addr = PLATFORM_ACLINT_MSWI_ADDR,
@@ -75,58 +51,76 @@ static struct aclint_mswi_data mswi = {
 static struct aclint_mtimer_data mtimer = {
     .mtime_freq = PLATFORM_ACLINT_MTIMER_FREQ,
     .mtime_addr = PLATFORM_ACLINT_MTIMER_ADDR +
-              ACLINT_DEFAULT_MTIME_OFFSET,
+                  ACLINT_DEFAULT_MTIME_OFFSET,
     .mtime_size = ACLINT_DEFAULT_MTIME_SIZE,
     .mtimecmp_addr = PLATFORM_ACLINT_MTIMER_ADDR +
-             ACLINT_DEFAULT_MTIMECMP_OFFSET,
+                     ACLINT_DEFAULT_MTIMECMP_OFFSET,
     .mtimecmp_size = ACLINT_DEFAULT_MTIMECMP_SIZE,
     .first_hartid = 0,
     .hart_count = PLATFORM_HART_COUNT,
     .has_64bit_mmio = TRUE,
 };
 
+unsigned long fw_platform_init(unsigned long arg0, unsigned long arg1,
+                               unsigned long arg2, unsigned long arg3,
+                               unsigned long arg4)
+{
+    const char *compatible;
+    void *fdt = (void *)arg1;
+    int root_offset, len;
+    struct plic_data plic_data;
+    unsigned long aclint_freq;
+    uint64_t clint_addr;
+    int rc;
+
+    root_offset = fdt_path_offset(fdt, "/");
+    if (root_offset >= 0) {
+        compatible = fdt_getprop(fdt, root_offset, "compatible", &len);
+        if (compatible &&
+            (0 == sbi_strncmp(compatible, "riscv-virtio", 12)))
+            qemu_mode = 1;
+    }
+
+    rc = fdt_parse_plic(fdt, &plic_data, "riscv,plic0");
+    if (!rc)
+        plic = plic_data;
+
+    rc = fdt_parse_timebase_frequency(fdt, &aclint_freq);
+    if (!rc)
+        mtimer.mtime_freq = aclint_freq;
+
+    rc = fdt_parse_compat_addr(fdt, &clint_addr, "riscv,clint0");
+    if (!rc) {
+        mswi.addr = clint_addr;
+        mtimer.mtime_addr = clint_addr + CLINT_MTIMER_OFFSET +
+                            ACLINT_DEFAULT_MTIME_OFFSET;
+        mtimer.mtimecmp_addr = clint_addr + CLINT_MTIMER_OFFSET +
+                               ACLINT_DEFAULT_MTIMECMP_OFFSET;
+    }
+
+    /* Return original FDT pointer */
+    return arg1;
+}
 
 static struct c910_regs_struct c910_regs;
 
 static int c910_early_init(bool cold_boot)
 {
     if (cold_boot) {
-        /* Load from boot core */
-        c910_regs.pmpaddr0 = csr_read(CSR_PMPADDR0);
-        c910_regs.pmpaddr1 = csr_read(CSR_PMPADDR1);
-        c910_regs.pmpaddr2 = csr_read(CSR_PMPADDR2);
-        c910_regs.pmpaddr3 = csr_read(CSR_PMPADDR3);
-        c910_regs.pmpaddr4 = csr_read(CSR_PMPADDR4);
-        c910_regs.pmpaddr5 = csr_read(CSR_PMPADDR5);
-        c910_regs.pmpaddr6 = csr_read(CSR_PMPADDR6);
-        c910_regs.pmpaddr7 = csr_read(CSR_PMPADDR7);
-        c910_regs.pmpcfg0  = csr_read(CSR_PMPCFG0);
-
-        c910_regs.mcor     = csr_read(CSR_MCOR);
-        c910_regs.mhcr     = csr_read(CSR_MHCR);
-        c910_regs.mccr2    = csr_read(CSR_MCCR2);
-        c910_regs.mhint    = csr_read(CSR_MHINT);
-        c910_regs.mxstatus = csr_read(CSR_MXSTATUS);
-
-        c910_regs.plic_base_addr = PLATFORM_PLIC_ADDR;//csr_read(CSR_PLIC_BASE);
-        c910_regs.clint_base_addr =
-            c910_regs.plic_base_addr + C910_PLIC_CLINT_OFFSET;
+        if (!qemu_mode) {
+            c910_regs.mcor = csr_read(CSR_MCOR);
+            c910_regs.mhcr = csr_read(CSR_MHCR);
+            c910_regs.mccr2 = csr_read(CSR_MCCR2);
+            c910_regs.mhint = csr_read(CSR_MHINT);
+            c910_regs.mxstatus = csr_read(CSR_MXSTATUS);
+        }
     } else {
-        /* Store to other core */
-        csr_write(CSR_PMPADDR0, c910_regs.pmpaddr0);
-        csr_write(CSR_PMPADDR1, c910_regs.pmpaddr1);
-        csr_write(CSR_PMPADDR2, c910_regs.pmpaddr2);
-        csr_write(CSR_PMPADDR3, c910_regs.pmpaddr3);
-        csr_write(CSR_PMPADDR4, c910_regs.pmpaddr4);
-        csr_write(CSR_PMPADDR5, c910_regs.pmpaddr5);
-        csr_write(CSR_PMPADDR6, c910_regs.pmpaddr6);
-        csr_write(CSR_PMPADDR7, c910_regs.pmpaddr7);
-        csr_write(CSR_PMPCFG0, c910_regs.pmpcfg0);
-
-        csr_write(CSR_MCOR, c910_regs.mcor);
-        csr_write(CSR_MHCR, c910_regs.mhcr);
-        csr_write(CSR_MHINT, c910_regs.mhint);
-        csr_write(CSR_MXSTATUS, c910_regs.mxstatus);
+        if (!qemu_mode) {
+            csr_write(CSR_MCOR, c910_regs.mcor);
+            csr_write(CSR_MHCR, c910_regs.mhcr);
+            csr_write(CSR_MHINT, c910_regs.mhint);
+            csr_write(CSR_MXSTATUS, c910_regs.mxstatus);
+        }
     }
 
     return 0;
@@ -159,7 +153,7 @@ static int platform_irqchip_init(bool cold_boot)
 {
     /* Delegate plic enable into S-mode */
     writel(C910_PLIC_DELEG_ENABLE,
-            (void *)c910_regs.plic_base_addr + C910_PLIC_DELEG_OFFSET);
+           (void *)plic.addr + C910_PLIC_DELEG_OFFSET);
 
     return 0;
 }
@@ -173,9 +167,9 @@ static int platform_ipi_init(bool cold_boot)
 
     /* Example if the generic ACLINT driver is used */
     if (cold_boot) {
-            ret = aclint_mswi_cold_init(&mswi);
-            if (ret)
-                    return ret;
+        ret = aclint_mswi_cold_init(&mswi);
+        if (ret)
+            return ret;
     }
 
     return aclint_mswi_warm_init();
@@ -202,19 +196,20 @@ static int platform_timer_init(bool cold_boot)
  * Platform descriptor.
  */
 const struct sbi_platform_operations platform_ops = {
-    .early_init     = platform_early_init,
-    .final_init     = platform_final_init,
-    .console_init   = fdt_serial_init,
-    .irqchip_init   = platform_irqchip_init,
-    .ipi_init       = platform_ipi_init,
-    .timer_init     = platform_timer_init
+    .early_init = platform_early_init,
+    .final_init = platform_final_init,
+    .console_init = fdt_serial_init,
+    .irqchip_init = platform_irqchip_init,
+    .ipi_init = platform_ipi_init,
+    .timer_init = platform_timer_init
 };
+
 const struct sbi_platform platform = {
-    .opensbi_version    = OPENSBI_VERSION,
-    .platform_version   = SBI_PLATFORM_VERSION(0x0, 0x01),
-    .name               = "k1-pro",
-    .features           = SBI_PLATFORM_DEFAULT_FEATURES,
-    .hart_count         = PLATFORM_HART_COUNT,
-    .hart_stack_size    = SBI_PLATFORM_DEFAULT_HART_STACK_SIZE,
-    .platform_ops_addr  = (unsigned long)&platform_ops
+    .opensbi_version = OPENSBI_VERSION,
+    .platform_version = SBI_PLATFORM_VERSION(0x0, 0x01),
+    .name = "k1-pro",
+    .features = SBI_PLATFORM_DEFAULT_FEATURES,
+    .hart_count = PLATFORM_HART_COUNT,
+    .hart_stack_size = SBI_PLATFORM_DEFAULT_HART_STACK_SIZE,
+    .platform_ops_addr = (unsigned long)&platform_ops
 };
