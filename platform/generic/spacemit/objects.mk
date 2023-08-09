28396033e40faac7a8771f54b4d9903caa9cc31b
zhouxl <zhouxl@spacemit.com>
2023-08-09 21:45:49 +0800

    move k1pro platform code to generic folder, reduce duplicate code
    Change-Id: If407afa5f96f809ab68f9584adf72cb4ec798a0e

---
+++ platform/generic/spacemit/objects.mk
@@ -0,0 +1,6 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+carray-platform_override_modules-$(CONFIG_PLATFORM_SPACEMIT_K1PRO) += spacemit_k1pro
+platform-objs-$(CONFIG_PLATFORM_SPACEMIT_K1PRO) += spacemit/k1pro.o
