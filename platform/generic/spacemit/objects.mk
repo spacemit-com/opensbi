99f42f8404c28ddaba991ad8d93a9608cd6a3e15
zhangmeng <zhangmeng.kevin@spacemit.com>
2023-08-11 08:53:16 +0800

    add k1-x platform support
    Change-Id: Ie5cbc300914dfe42a41dddf7ea7d9e09801cbee3

--- platform/generic/spacemit/objects.mk
+++ platform/generic/spacemit/objects.mk
@@ -4,3 +4,6 @@
 
 carray-platform_override_modules-$(CONFIG_PLATFORM_SPACEMIT_K1PRO) += spacemit_k1pro
 platform-objs-$(CONFIG_PLATFORM_SPACEMIT_K1PRO) += spacemit/k1pro.o
+
+carray-platform_override_modules-$(CONFIG_PLATFORM_SPACEMIT_K1X) += spacemit_k1x
+platform-objs-$(CONFIG_PLATFORM_SPACEMIT_K1X) += spacemit/k1x.o
