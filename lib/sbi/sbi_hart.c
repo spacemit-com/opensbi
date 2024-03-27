ce6bacbf605ad67f1234f2ed15076e12067f38d4
zhangmeng <zhangmeng.kevin@spacemit.com>
2024-03-27 21:23:33 +0800

    fence: enhance fence operation to fix a bug in dmapool
        dmapool allocate a page by dma_alloc_coherent(), which PMA attribute is IO
        then the flow is:
            cpu0:
                 spin_lock_irqsave()
                 *(int *)vaddr = offset
                 spin_lock_irqrestore()
            cpu1:
                 spin_lock_ireqsave()
                 offset = *(int *)vaddr
                 spin_lock_irqrestore()
        cpu 1 may get a wrong offset from *(int *)vaddr, that before cpu0 set *(int *)vaddr
        because there is no coherent with cpu0/cpu1 on the IO memory area
        when set the CSR, the fence instruction in spin_lock_irqsave/restore will force sync
        data to memory.
    
    Change-Id: I982aa6b46fc89643a83bb6b35a98484012dd756b

--- lib/sbi/sbi_hart.c
+++ lib/sbi/sbi_hart.c
@@ -819,6 +819,7 @@ sbi_hart_switch_mode(unsigned long arg0, unsigned long arg1,
 	}
 
 	csr_write(CSR_TCMCFG, 1);
+	csr_set(CSR_FENCECFG, (1<<9));
 
 	register unsigned long a0 asm("a0") = arg0;
 	register unsigned long a1 asm("a1") = arg1;
